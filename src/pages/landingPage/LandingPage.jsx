1f3ba1fe046b2d2524c994098ba4a763833b9dec
Sankha Rathnayake <2017cs143@stu.ucsc.cmb.ac.lk>
2022-09-23 15:01:00 +0530

    landingPage done

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -4,9 +4,20 @@ import React from 'react'
 // styling
 import './LandingPage.scss'
 
+// assets
+import landingImage from '../../assets/Landing page image.svg'
+
+
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className='LandingPage'>
+        <h1>Imagine if </h1>
+        <h1>Snapchat</h1>
+        <h1>had events</h1>
+        <p>Easily host and share events with your friends across any social media.</p>
+        <img src={landingImage} />
+        <button>🎉 Create my event</button>
+    </div>
   )
 }
 
